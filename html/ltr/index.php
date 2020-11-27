46889dcd00a8e16a01e122e04c36cb79096ec7e3
Junior Javier Sanchez <juniorsc750@gmail.com>
2020-11-27 12:54:53 -0500

    Update index.php

--- html/ltr/index.php
+++ html/ltr/index.php
@@ -281,7 +281,7 @@
 
     <footer class="footer footer-static footer-light navbar-border">
       <p class="clearfix text-muted text-sm-center mb-0 px-2">
-        <span class="float-md-left d-xs-block d-md-inline-block">Copyright &copy; 2017
+        <span class="float-md-left d-xs-block d-md-inline-block">Copyright &copy; 2020
           <a href="#" target="_blank" class="text-bold-800 grey darken-2">PROJANET </a>. </span>
       </p>
     </footer>
